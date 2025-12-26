7553477beac1c96638133359dd3a4976592cbb1e
BLE <u.ble@orange.fr>
2025-12-26 09:41:19 +0100

    add mse

---
+++ leet_gpu/24_mse.cu
@@ -0,0 +1,70 @@
+#include <cuda_runtime.h>
+
+__global__ void difference_partial(const float* predictions, const float* targets, float* partial,int N){
+    extern __shared__ float sdata[];
+    int tid = threadIdx.x;
+    int idx = blockIdx.x * blockDim.x + tid; 
+    int stride = blockDim.x * gridDim.x;
+
+    float sum = 0.0f;
+
+    for (int i = idx; i < N; i += stride) {
+        float diff = predictions[i] - targets[i];
+        sum += diff * diff;
+    }
+
+    sdata[tid] = sum;
+    __syncthreads();
+
+    for (int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        partial[blockIdx.x] = sdata[0];
+    }
+}
+
+__global__ void reduce_final(float* data, int N,int original_N) {
+    extern __shared__ float sdata[];
+    int tid = threadIdx.x;
+
+    if (tid < N)
+        sdata[tid] = data[tid];
+    else
+        sdata[tid] = 0.0f;
+
+    __syncthreads();
+
+    for (int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s)
+            sdata[tid] += sdata[tid + s];
+        __syncthreads();
+    }
+
+    if (tid == 0)
+        data[0] = sdata[0]/original_N;
+}
+// predictions, targets, mse are device pointers
+extern "C" void solve(const float* predictions, const float* targets, float* mse, int N) {
+    int threads = 256;
+    int blocks = min(1024, (N + threads - 1) / threads);
+
+    float* d_partial;
+    cudaMalloc(&d_partial, blocks * sizeof(float));
+
+    difference_partial<<<blocks, threads, threads * sizeof(float)>>>(
+        predictions, targets, d_partial, N
+    );
+
+    reduce_final<<<1, 1024, 1024 * sizeof(float)>>>(
+        d_partial, blocks,N
+    );
+
+    cudaMemcpy(mse, d_partial, sizeof(float), cudaMemcpyDeviceToDevice);
+
+    cudaFree(d_partial);
+}
